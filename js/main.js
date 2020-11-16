4a5983546ca8c11400c556feba9acdad940d598b
Elaine Esmenda <elaineesmenda@Elaine.local>
2020-11-15 22:02:27 -0500

    add lb

--- js/main.js
+++ js/main.js
@@ -8,23 +8,38 @@ import {fetchData} from "./modules/TheDataMiner.js";
         alert("Something has gone horribly, horribly wrong");
     }
 
-    function handleDataSet(data) {
+    // function handleDataSet(data) {}
     // populate a lightbox with this data and then open it
-    let lightbox = document.querySelector('.lightbox'),
-        closeBTN = document.querySelector('.closeBTN'),
-        clickBTN = document.getElementById('nav-item');
-        
-    function showLB(){
-        lightbox.classList.add("showLB");
-    }
-            
-    function hideLB(){
-        lightbox.classList.remove("hideLB");
+    // let lightbox = document.querySelector('.lightbox'),
+    let clickButton = document.querySelectorAll('[data-click-target]'),
+        closeButton = document.querySelectorAll('[data-close-button]'),
+        overlay = document.getElementById('overlay');
+
+    function openLB() {
+        if (lightbox == null) return
+        lightbox.classList.add("openLB")
+        overlay.classList.add("openLB")
     }
 
-    clickBTN.addEventListener("click",showLB);
-    closeBTN.addEventListener("click",hideLB);
+    function closeLB() {
+        if (lightbox == null) return
+        lightbox.classlist.remove("openLB")
+        overlay.classlist.remove("openLB")
     }
+
+    clickButton.forEach(button => {
+        button.addEventListener("click", () => {
+            const lightbox = document.querySelectorAll(button.dataset.clickTarget)
+            openLB()
+        })
+    })
+
+    closeButton.forEach(button => {
+        button.addEventListener("click", () => {
+            const lightbox = button.closest('lightbox')
+            closeLB()
+        })
+    })
 
     function retrieveProjectInfo(event) {
         // test for an ID
@@ -37,7 +52,7 @@ import {fetchData} from "./modules/TheDataMiner.js";
         // check for an ID and if there isnt one, then dont try the fetch call because it'll  break (the PHP choke)
         if (!event.target.id) { return }
 
-        fetchData(`./includes/index.php?id=${event.target.id}`).then(data => retrieveProjectInfo(event)).catch(err => console.log(err));
+        fetchData(`./includes/index.php?id=${event.target.id}`).then(data => console.log(data)).catch(err => console.log(err));
     }
 
     function renderPortfolioThumbnails(thumbs) {
@@ -49,7 +64,7 @@ import {fetchData} from "./modules/TheDataMiner.js";
                 currentUserText = currentUser.querySelector('.user').children;
 
              currentUserText[1].src = `images/${thumbs[user].Image}`;
-             currentUserText[1].id = thumbs[user].id;
+             currentUserText[1].id = thumbs[user].ID;
             // add this new user to the view
              currentUser.addEventListener("click", retrieveProjectInfo);
              userSection.appendChild(currentUser);
